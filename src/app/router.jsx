95d92c96c20503e87b8e58c4fd3e84f6664151f8
YoussefNabil77 <youssefwaer145@gmail.com>
2026-01-24 17:16:20 +0200

    Add profile routes

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Landing from "../pages/Landing";
 import AuthPage from "../features/auth/pages/AuthPage";
+import ProfilePage from "../features/profile/pages/ProfilePage";
+import UserProfilePage from "../features/profile/pages/UserProfilePage";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Landing /> },
   { path: "/login", element: <AuthPage /> },
   { path: "/signup", element: <AuthPage /> },
+  { path: "/profile", element: <ProfilePage /> },
+  { path: "/profile-view", element: <UserProfilePage /> },
 ]);
