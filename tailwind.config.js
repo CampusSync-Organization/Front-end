5452cd57aa6bf2639daa61c8dbf260652d2b5d9f
YoussefMohamedSafwat <safottanew2@gmail.com>
2026-02-03 19:02:03 +0200

    configured tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#14213D",
+        secondary: "#FCA311",
+        "neutral-light": "#E5E5E5",
+      },
+    },
   },
   plugins: [],
 };
