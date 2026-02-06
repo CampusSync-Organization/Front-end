6d9e0dccd734b677a5badff6670adea120adedd5
YoussefNabil77 <youssefwaer145@gmail.com>
2026-02-06 23:48:17 +0200

    Add feature and fix router imports

--- src/app/router/router.jsx
+++ src/app/router/router.jsx
@@ -1,15 +1,15 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import AppLayout from "../../components/AppLayout";
-import AuthGuard from "../../components/AuthGuard";
-import Landing from "../../features/pages/Landing";
-import LoginPage from "../../features/auth/pages/LoginPage";
-import SignUpPage from "../../features/auth/pages/SignUpPage";
-import AssessmentPage from "../../features/assessment/pages/AssessmentPage";
-import HomePage from "../../features/home/pages/HomePage";
-import ProfilePage from "../../features/profile/pages/ProfilePage";
-import UserProfilePage from "../../features/profile/pages/UserProfilePage";
-import ChatMainPage from "../../features/chat/pages/ChatMainPage";
-import EventsAndCommunitiesLayoutPage from "../../features/events-communities/pages/EventsAndCommunitiesLayoutPage";
+import AppLayout from "../../components/AppLayout.jsx";
+import AuthGuard from "../../components/AuthGuard.jsx";
+import Landing from "../../features/pages/Landing.jsx";
+import LoginPage from "../../features/auth/pages/LoginPage.jsx";
+import SignUpPage from "../../features/auth/pages/SignUpPage.jsx";
+import AssessmentPage from "../../features/assessment/pages/AssessmentPage.jsx";
+import HomePage from "../../features/home/pages/HomePage.jsx";
+import ProfilePage from "../../features/profile/pages/ProfilePage.jsx";
+import UserProfilePage from "../../features/profile/pages/UserProfilePage.jsx";
+import ChatMainPage from "../../features/chat/pages/ChatMainPage.jsx";
+import EventsAndCommunitiesLayoutPage from "../../features/events-communities/pages/EventsAndCommunitiesLayoutPage.jsx";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Landing /> },
